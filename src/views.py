c6de61e13da6ffc493d000980499a6fae94d4e7c
Ekaterina <kkarmyjjk@gmail.com>
2025-04-29 16:51:54 +0300

    wrote functions for views

--- src/views.py
+++ src/views.py
@@ -1,9 +1,14 @@
 import json
+import os
+import re
 import requests
 import pandas as pd
-from datetime import datetime, time
+from datetime import datetime, time, timedelta
 import logging
 from pathlib import Path
+from pprint import pprint
+
+from src.utils import read_transactions_from_excel
 
 logger = logging.getLogger(__name__)
 file_handler = logging.FileHandler('../logs/views.log')
@@ -55,3 +60,155 @@ def greetings(actual_time: str) -> str:
         logger.error("Передано неверное время")
 
         raise ValueError("Неверный формат времени")
+
+def sort_by_date(operations: list[dict], input_date: str) -> str | list[dict]:
+    """
+    Функция, получающая список словарей с операциями и дату, возвращающая список, отфильтрованный
+    по дате с начала месяца, на который выпадает входящая дата, по входящую дату.
+    """
+    pattern = re.compile(r"(\d{2})\.(\d{2})\.(\d{4})")
+
+    result = []
+
+    logger.info("Проверяем переданную дату на корректный формат")
+
+    if input_date and pattern.fullmatch(input_date):
+
+        logger.info("Формат даты корректный")
+
+        day_int = int(input_date[:2])
+
+        input_date_obj = datetime.strptime(input_date, "%d.%m.%Y").date()
+
+        start = input_date_obj - timedelta(days=(day_int - 1))
+        stop = input_date_obj
+        logger.info("Фильтруем операции по дате")
+        for operation in operations:
+            operation_date_obj = datetime.strptime(operation["Дата операции"], "%d.%m.%Y %H:%M:%S").date()
+
+            if start <= operation_date_obj <= stop:
+                result.append(operation)
+
+    else:
+
+        logger.warning("Введена неверная дата")
+
+        print("Введена неверная дата. Введите дату в формате ДД.ММ.ГГГГ")
+
+    return result
+
+def get_card_info(operations_list: list[dict]) -> list[dict]:
+    """
+    Функция, принимающая список операций и возвращающая список словарей с данными о картах:
+    последние 4 цифры карты, общая сумма расходов, кешбэк (1 рубль на каждые 100 рублей) в формате
+    [{"last_digits": "4 последние цифры номера карты",
+      "total_spent": сумма расходов,
+      "cashback": кэшбек},
+    {...}]
+    """
+    card_data = {}
+    pattern = re.compile(r"\*\d{4}")
+
+    logger.info("Определяем номер карты")
+    for operation in operations_list:
+
+        if isinstance(operation["Номер карты"], str) and pattern.fullmatch(operation["Номер карты"]):
+            if "Сумма операции" in operation and "Статус" in operation:
+
+                card_number = operation["Номер карты"][1:]
+                amount = operation["Сумма операции"]
+
+                logger.info("Проверяем статус каждой операции")
+
+                if operation["Статус"] == "OK" and float(amount) < 0:
+                    if card_number not in card_data:
+                        logger.info("Считаем сумму операций по каждой карте")
+
+                        card_data[card_number] = 0.0
+
+                    card_data[card_number] += abs(float(amount))
+
+    result = []
+
+    logger.info("Формируем результат с данными о картах")
+
+    for card_num, data in card_data.items():
+        last_digits = card_num
+        total_spent = data
+        cashback = total_spent * 0.01
+
+        result.append(
+            {"last_digits": last_digits, "total_spent": round(total_spent, 2), "cashback": round(cashback, 2)}
+        )
+
+    return result
+
+def load_user_settings(filepath='user_settings.json'):
+    """Функция для загрузки пользовательских настроек. На вход принамается путь к файлу(по умолчанию 'user_settings.json')"""
+    with open(filepath, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+def get_top_transactions(operations: list[dict]) -> list[dict]:
+    """
+    Функция, принимающая список словарей с операциями и возвращающая список из топ-5 транзакций по сумме
+    в формате:
+    [{"date": "дата",
+      "amount": сумма,
+      "category": "категория",
+      "description": "описание"},
+    {...}]
+    """
+    result = []
+
+    negative_transactions = [
+        operation for operation in operations if "Сумма операции" in operation and operation["Сумма операции"] < 0
+    ]
+
+    logger.info("Определяем топ-5 операций по сумме")
+
+    top_5 = sorted(negative_transactions, key=lambda x: abs(x["Сумма операции"]), reverse=True)[:5]
+
+    logger.info("Формируем данные для получения топ-5 операций в нужном формате")
+
+    for top in top_5:
+        date = top["Дата операции"].split()[0]
+        amount = abs(top["Сумма операции"])
+        category = top["Категория"]
+        description = top["Описание"]
+
+        info_top = {"date": date, "amount": round(amount, 2), "category": category, "description": description}
+        result.append(info_top)
+
+    return result
+
+
+def get_currency_rates(currencies: list):
+    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_qHKbYCrpNsZ69CIGRL2pEVREk5s7o2afg4QkgFJB"
+    headers = {
+        "apikey": os.getenv('API_KEY')
+    }
+    response = requests.get(url, headers=headers, data={})
+
+    result = response.json()
+    print(result)
+    results = []
+    usd_to_rub = result['data']['RUB']['value']  # курс USD к RUB
+
+    for res in result:
+        print(res)
+        print()
+        print(currencies)
+        if res in currencies:
+            currency_to_rub = result['data'][res]['value']
+            print(currency_to_rub)
+    # for currency_code, currency_data in result['data'].items():
+    #     rate = currency_data.get('value')
+    #     if rate and rate != 0:
+    #         currency_to_rub = round(usd_to_rub / rate, 2)  # сколько 1 единица валюты стоит в рублях
+    #         results.append({"currency": currency_code, "rate": currency_to_rub})
+    # return results
+
+#'EUR': {'code': 'EUR', 'value': 0.8764201365}
+
+# tp = read_transactions_from_excel("../data/operations.xlsx")
+pprint(get_currency_rates(["EUR"]))
