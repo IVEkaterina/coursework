fa86b6b27353c1e1030212828b99f970a78326b0
Ekaterina <kkarmyjjk@gmail.com>
2025-05-02 02:08:25 +0300

    finished function for reports

---
+++ src/decorators.py
@@ -0,0 +1,39 @@
+import logging
+from functools import wraps
+
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('../logs/views.log')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+def decorator_record_file(file_name):
+    """
+    Декоратор, который записывает результат выполнения функции в JSON файл. Принимает на вход имя файла.
+    """
+
+    def wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+
+            df = func(*args, **kwargs)
+
+            logger.info('Проверка: являются ли данные датафреймом')
+
+            if isinstance(df, pd.DataFrame):
+
+                logger.info('Запись отчёта в файл')
+
+                df.to_json(file_name, orient="records", lines=True, force_ascii=False)
+
+            else:
+                logger.error('Данные не являются датафреймом. В файл записаны не будут')
+
+            return df
+
+        return inner
+
+    return wrapper
